a794641b96ef370df1785a043ba97495770edc65
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2024-10-31 12:19:45 +0200

    add saving to file fssp clients

--- src/main.js
+++ src/main.js
@@ -3,21 +3,39 @@ import path from 'path';
 import { parseExcel, saveExcel } from './services/excel.service.js';
 import { filterClients, getFsspAndFilterClients } from './services/fssp.service.js';
 import { logger } from './services/logger.service.js';
+import * as fs from 'node:fs';
+
+async function getFsspClients(filePath) {
+  try {
+    await fs.promises.access(filePath);
+
+    return await parseExcel(filePath);
+  } catch (error) {
+    return [];
+  }
+}
 
 export async function parseFssp() {
   try {
     const inputFilePath = path.join(process.cwd(), 'assets', 'input', 'list.xlsx');
     const outputFilePath = path.join(process.cwd(), 'assets', 'output', 'list.xlsx');
-    const entries = await parseExcel(inputFilePath);
-    const clients = await filterClients(entries);
+    const outputFsspFilePath = path.join(process.cwd(), 'assets', 'output', 'fssp-list.xlsx');
+    const clients = await filterClients(await parseExcel(inputFilePath));
     const filteredClients = await getFsspAndFilterClients(clients, 2000);
 
     await saveExcel(
       [
-        ...await parseExcel(outputFilePath),
-        ...filteredClients.filteredEntries
+        ...await getFsspClients(outputFilePath),
+        ...filteredClients.filteredClients
       ],
       outputFilePath
+    );
+    await saveExcel(
+      [
+        ...await getFsspClients(outputFsspFilePath),
+        ...filteredClients.fsspClients
+      ],
+      outputFsspFilePath
     );
     await saveExcel(clients.slice(filteredClients.count), inputFilePath);
 
